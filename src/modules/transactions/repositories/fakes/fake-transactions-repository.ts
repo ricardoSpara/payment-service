da966ae34d52b9b7aea5182d72dea48abd960b60
Ricardo Sparapan <ricardo.sparapan@pontaltech.com.br>
2021-05-10 20:28:37 -0300

    feat: add fake transaction

---
+++ src/modules/transactions/repositories/fakes/fake-transactions-repository.ts
@@ -0,0 +1,27 @@
+import { ICreateTransactionDTO } from "@modules/transactions/dtos/icreate-transaction-dto";
+import { Transaction } from "@modules/transactions/entities/transaction";
+import { ITrasanctionsRepository } from "@modules/transactions/repositories/contracts/itransactions-repository";
+
+class FakeTransactionsRepository implements ITrasanctionsRepository {
+  public transactions: Transaction[] = [];
+
+  async create({
+    value,
+    payer_id,
+    payee_id,
+  }: ICreateTransactionDTO): Promise<Transaction> {
+    const transaction = new Transaction();
+
+    Object.assign(transaction, {
+      value,
+      payer_id,
+      payee_id,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export { FakeTransactionsRepository };
